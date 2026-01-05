1f9699c20577b65706b67a97ae8d33472e5351ad
Roxana <cristian.tolcea93@gmail.com>
2026-01-06 00:01:02 +0200

    args parser initial implementation

--- code/process_analyzer.c
+++ code/process_analyzer.c
@@ -1,16 +1,24 @@
-#include "stdio.h"
 #include "process_snapshot.h"
+#include "args_parser.h"
 
-int main(void)
+
+ap_arguments gArguments;
+
+
+
+int main(int argc, char **argv)
 {
-	process_snapshot_status l_process_status = process_snapshot_error;
-	l_process_status = process_snapshot_initialize();
+	parse_args_status l_parse_status  = ap_parse_args(argc, argv, &gArguments);
 
-	if(process_snapshot_success == l_process_status)
+	if(parse_args_ok == l_parse_status)
 	{
-		collect_snapshot();
-		process_snapshot_deinit();
+		process_snapshot_status l_process_status = process_snapshot_error;
+		l_process_status = process_snapshot_initialize();
+		if(process_snapshot_success == l_process_status)
+		{
+			collect_snapshot();
+			process_snapshot_deinit();
+		}
 	}
-
 	return 0;
 }
